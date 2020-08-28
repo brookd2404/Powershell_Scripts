92022374c0f1f4ed8322a6d3e291b8df9edc9f41
David Brook <DABR17@hscic.gov.uk>
2020-08-28 08:30:44 +0100

    Updated Autopilot Script

--- MS-Graph/Autopilot/Enroll_to_Autopliot_Unattended.ps1
+++ MS-Graph/Autopilot/Enroll_to_Autopliot_Unattended.ps1
@@ -1,3 +1,82 @@
+  
+<#PSScriptInfo
+ 
+.VERSION 2.0
+ 
+.AUTHOR David Brook
+ 
+.COMPANYNAME EUC365
+ 
+.COPYRIGHT
+ 
+.TAGS Autopilot; Intune; Mobile Device Management
+ 
+.LICENSEURI
+ 
+.PROJECTURI 
+ 
+.ICONURI
+ 
+.EXTERNALMODULEDEPENDENCIES
+ 
+.REQUIREDSCRIPTS
+ 
+.EXTERNALSCRIPTDEPENDENCIES
+ 
+.RELEASENOTES
+Version 2.0: Added the ability to make the script accept command line arguments for just the Hash and also allow Group Tags
+Version 1.0: Original published version.
+ 
+#>
+
+
+<#
+.SYNOPSIS
+This script will import devices to Microsoft Endpoint Manager Autopilot using the device's hardware hash.  
+ 
+.DESCRIPTION
+This script will import devices to Microsoft Endpoint Manager Autopilot using the device's hardware hash with the added capability of been able to add a Group Tag.
+
+.PARAMETER MSGraphVersion
+The Version of the MS Graph API to use
+Default: Beta
+e.g: 1.0
+
+.PARAMETER MsGraphHost
+The MS Graph API Host
+Default: graph.microsoft.com
+
+.PARAMETER ClientID
+This is the Azure AD App Registration Client ID
+
+.PARAMETER ClientSecret
+This is the Azure AD App Registration Client Secret
+
+.PARAMETER TenantId
+Your Azure Tenant ID
+
+.PARAMETER Hash
+This parameter is to be used if you want to import a specific hash from either a file or copying and pasting from an application. 
+ 
+.PARAMETER GroupTag
+This Parameter is to be used if you want to Tag your devices with a specific group tag. 
+
+.EXAMPLE
+.\Enroll_to_Autopliot_Unattended.ps1 -ClientID "<Your Client ID>" -Client Secret "<YourClientSecret>" -TenantID "<YourTenantID>"
+
+This will enroll the device it is running on to Autopilot, Please note this will need to be done as an administrator
+ 
+.EXAMPLE
+.\Enroll_to_Autopliot_Unattended.ps1 -ClientID "<Your Client ID>" -Client Secret "<YourClientSecret>" -TenantID "<YourTenantID>" -GroupTag "Sales Device"
+
+This will enroll the device it is running on to Autopilot with a Group Tag of Sales Device, Please note this will need to be done as an administrator
+ 
+.EXAMPLE
+.\Enroll_to_Autopliot_Unattended.ps1 -ClientID "<Your Client ID>" -Client Secret "<YourClientSecret>" -TenantID "<YourTenantID>" -Hash "<A Hash>"
+
+This will enroll the inputed deivce Hash to Autopilot, this can be done against a group of CSV files etc. 
+  
+#>
 param(
     [Parameter(DontShow = $true)]
     [string]
