44c7f21d836ed3475f736810055b134a58fe73d6
David Brook <david.brook@poweronplatforms.com>
2023-02-15 11:14:20 +0000

    Update to be able to run as script

--- MS-Graph/WUfB DS/Update-PatchDeferrals.ps1
+++ MS-Graph/WUfB DS/Update-PatchDeferrals.ps1
@@ -1,14 +1,25 @@
-<#
-.SYNOPSIS
-    This function is to be used to update the Patch Deferral on Update Policies.
-.NOTES
-    This has only been tested for the commercial driver and firmware updates.
-.EXAMPLE
-    Update-PatchDeferrals -updatePolicyID <id> -deferralTime PT1D
-    Explanation of the function or its result. You can include multiple The deferral time must be in the ISO8601 format. 
-#>
+param (
+    # The Update Policy ID
+    [Parameter(Mandatory = $true)]
+    [string]
+    $updatePolicyID,
+    # ISO8601 Timeformat for Deferral
+    [Parameter(Mandatory = $true)]
+    [string]
+    $deferralTime
+)
+    
 
 function Update-PatchDeferrals {
+    <#
+    .SYNOPSIS
+        This function is to be used to update the Patch Deferral on Update Policies.
+    .NOTES
+        This has only been tested for the commercial driver and firmware updates.
+    .EXAMPLE
+        Update-PatchDeferrals -updatePolicyID <id> -deferralTime PT1D
+        Explanation of the function or its result. You can include multiple The deferral time must be in the ISO8601 format. 
+    #>
     [CmdletBinding()]
     param (
         # The Update Policy ID
@@ -45,3 +56,7 @@ function Update-PatchDeferrals {
         Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/admin/windows/updates/updatePolicies/$updatePolicyID" -Method PATCH -Body $paramBody
     }
 }
+
+Connect-MgGraph -Scopes "WindowsUpdates.ReadWrite.All" -ContextScope CurrentUser
+
+Update-PatchDeferrals -updatePolicyID $updatePolicyID -deferralTime $deferralTime
