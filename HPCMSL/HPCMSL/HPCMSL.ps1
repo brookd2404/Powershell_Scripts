347990ea668c6766667c74e55b457d92be0634cb
= <=>
2024-06-07 11:08:34 +0100

    Add HPCMSL Install

---
+++ HPCMSL/HPCMSL/HPCMSL.ps1
@@ -0,0 +1,34 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    #Download center URL: https://www.hp.com/us-en/solutions/client-management-solutions/download.html
+    $downloadURL = "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.7.1.exe",
+    [Parameter()]
+    [string]
+    $tempSPPath = "$env:TEMP\HPSP",
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("Install", "Uninstall")]
+    [array]
+    $Action = @("Install", "Uninstall")
+)
+
+
+switch ($Action)
+{
+    Install
+    {
+        $installerPath = Join-Path -Path $tempSPPath -ChildPath "HP-CMSL.exe"
+
+        $webClient = New-Object System.Net.WebClient
+        $webClient.DownloadFile($downloadURL, $installerPath)
+
+        Start-Process -FilePath $installerPath -ArgumentList "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES" -Wait
+
+    }
+    Uninstall
+    {
+        $uninstallerPath = "$env:ProgramFiles\WindowsPowerShell\HP.CMSL.UninstallerData\unins000.exe"
+        Start-Process -FilePath $uninstallerPath -ArgumentList "/VERYSILENT NORESTART /SUPPRESSMSGBOXES" -Wait
+    }
+}
